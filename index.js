fd46f02e898358d732997bf661a849b4f81c8b95
Namtazmo <truongvannam@hotmail.com>
2026-07-23 21:33:36 +0700

    su dung morgan

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express()
 const port = 3000
-
+var morgan = require('morgan')
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
